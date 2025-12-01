1ee89e4e5931f160160bb26f9ed08f3fad84fac4
sellayspy <riki.fauziagaming@gmail.com>
2025-12-01 19:38:25 +0700

    menambah file nixpacks.toml

--- main.py
+++ main.py
@@ -1,6 +1,51 @@
-def main():
-    print("Hello from model-data-database!")
+from flask import Flask, jsonify, request
+import pandas as pd
+import joblib
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+load_model = joblib.load("models/model_diabetes.pkl")
+
+load_scaler = joblib.load("models/standart_scaler.pkl")
+
+columns = [
+    "Pregnancies",
+    "Glucose",
+    "BloodPressure",
+    "SkinThickness",
+    "Insulin",
+    "BMI",
+    "DiabetesPedigreeFunction",
+    "Age",
+]
+
+
+@app.route("/")
+def index():
+    return jsonify(
+        {"meta": {"status": "success", "message": "Welcome to Api"}, "data": None}
+    )
+
+
+@app.route("/api/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+
+    x_input = pd.DataFrame([data], columns=columns)
+
+    x_input_scaled = load_scaler.transform(x_input)
+
+    prediction = load_model.predict(x_input_scaled)
+
+    return jsonify(
+        {
+            "meta": {"status": "success", "message": "Prediction"},
+            "data": prediction.tolist()[0],
+        }
+    )
 
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
