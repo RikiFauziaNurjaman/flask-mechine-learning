3b8f217d0c3d51e15fec4790e15676ba1b0d99fd
sellayspy <riki.fauziagaming@gmail.com>
2025-12-11 18:25:30 +0700

    Tambahan

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from flask import Flask, jsonify, request
 import pandas as pd
+import tensorflow as tf
+import numpy as np
+import io
+
 import joblib
 from flask_cors import CORS
+from flask import Flask, jsonify, request
+
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet import preprocess_input
 
 app = Flask(__name__)
 CORS(app)
 
 load_model = joblib.load("models/model_diabetes.pkl")
+load_model_rps = tf.keras.models.load_model("models/RPS.keras")
 
 load_scaler = joblib.load("models/standart_scaler.pkl")
 
@@ -20,6 +28,8 @@ columns = [
     "DiabetesPedigreeFunction",
     "Age",
 ]
+
+rps_class_names = ["Paper", "Rock", "Scissors"]
 
 
 @app.route("/")
@@ -43,6 +53,37 @@ def predict():
         {
             "meta": {"status": "success", "message": "Prediction"},
             "data": prediction.tolist()[0],
+        }
+    )
+
+
+@app.route("/api/predict-rps", methods=["POST"])
+def predict_rps():
+    if "file" not in request.files:
+        return jsonify(
+            {
+                "meta": {"status": 400, "message": "No file part in the request"},
+            }
+        )
+
+    file = request.files["file"]
+    img_bytes = io.BytesIO(file.read())
+
+    img = image.load_img(img_bytes, target_size=(150, 150))
+    img_array = image.img_to_array(img)
+    img_array = preprocess_input(img_array)
+    img_batch = np.expand_dims(img_array, axis=0)
+
+    predictions = load_model_rps.predict(img_batch)
+    predicted_class_index = np.argmax(predictions)
+
+    return jsonify(
+        {
+            "meta": {"status": 200, "message": "Prediction Successful"},
+            "data": {
+                "prediction": rps_class_names[predicted_class_index],
+                "probability": f"{np.max(predictions) * 100:.2f}%",
+            },
         }
     )
 
